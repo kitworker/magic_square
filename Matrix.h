1906d342b077f06ed1192cce27411f13b3c895e5
kitworker <kasyanov.nn@gmail.com>
2023-11-11 15:04:44 -0500

    Created unit test

---
+++ Matrix.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+#include <array>
+
+/**
+ *
+ * одномерные Массивы как регулярные структуры
+ * А поля структур как стек (объемный 3d!) массива
+ */
+class Matrix
+{
+public:
+    struct Number
+    {
+        int index;
+        int value;
+        Number() = default; // @suppress("Class members should be properly initialized")
+        explicit Number(int index, int value)
+            : index(index)
+            , value(value)
+        {
+
+        }
+
+        bool operator==(const Number& num) const
+        {
+            return num.value == value;
+        }
+    };
+
+    //	using Mag8 = std::array<int>; // int[8][8];
+    //const int DIM = 2;
+    using MagX = std::array<std::array<Number, 2>, 2>;
+public:
+    Matrix();
+
+    void addItem(int row, int column, int value);
+    Number getItem(int index, int row, int column);
+    MagX getMatrix(int index) const;
+
+    std::vector<MagX> getMatrixes() const;
+
+private:
+    void addNumber(int row, int column, const Number& number);
+    void init();
+
+private:
+    std::vector<MagX> m_matrixes;
+    int m_index;
+    //	std::array<std::array<int>> multiArray;
+};
